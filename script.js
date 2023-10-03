8fd2a793dfed916fcd4628c3611b1cf3411a9531
liane4323 <146861765+liane4323@users.noreply.github.com>
2023-10-03 11:54:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Lian Edhah"; // update the part BETWEEN the "quotes"
+let userName = "liane4323"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
